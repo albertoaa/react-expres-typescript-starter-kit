0f7f69a1c7df767d84553f2ee7c11bf64219296f
Alberto Aragón Alvarez <ronin8503@gmail.com>
2021-09-18 08:02:14 -0500

    displaying and styling list of customers

--- client/src/components/customers.tsx
+++ client/src/components/customers.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Customer } from '../models/Customer';
 import '../scss/customers.scss';
 
 export const Customers: React.FC = () => {
+  const [customers, setCustomers] = useState<Customer[]>([]);
+
+  const fetchCustomers = async () => {
+    const customerQuery = await fetch('/api/customers');
+    const customers = await customerQuery.json();
+    setCustomers(customers);
+  };
+
+  useEffect(() => {
+    fetchCustomers();
+  }, []);
   return (
     <div>
       <h1>Customers</h1>
+      <div>
+        <ul>
+          {customers.map((customer) => (
+            <li>{customer.firstName}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
